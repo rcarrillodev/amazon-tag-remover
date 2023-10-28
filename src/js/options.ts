c7c353628343b580bb72fb6fd1194df6940f2f3d
Rafael Carrillo <rcarrillorosario@gmail.com>
2023-10-27 18:07:10 -0700

    WIP : Add whitelist

--- src/js/options.ts
+++ src/js/options.ts
@@ -5,13 +5,41 @@ function saveOptions(e: Event) {
 	storage.set({
 		disableNotifications: document.querySelector<HTMLInputElement>('#disable-notifications')!.checked,
 	});
+	if (document.querySelector<HTMLInputElement>('#referral')?.value) {
+		let result;
+		storage.get('whiteList').then((result) => {
+			if (!result) {
+				storage.set({
+					whiteList: document.querySelector<HTMLInputElement>('#referral')?.value,
+				});
+				return;
+			}
+			let whiteList = result.whiteList.split(',');
+			whiteList.push(document.querySelector<HTMLInputElement>('#referral')?.value);
+			storage.set({
+				whiteList: whiteList.toString(),
+			});
+		});
+	}
 }
 
 function restoreOptions() {
 	function setCurrentChoice(result: any) {
 		document.querySelector<HTMLInputElement>('#disable-notifications')!.checked = result.disableNotifications;
 	}
-
+	function showWhiteList(result: any) {
+		if (!result.whiteList) {
+			return;
+		}
+		console.log(result.whiteList.split(','));
+		let r = result.whiteList.split(',');
+		r.forEach((element: string) => {
+			let item = document.createElement('li');
+			item.appendChild(document.createTextNode(element));
+			document.querySelector<HTMLUListElement>('#whitelist')?.appendChild(item);
+		});
+	}
+	storage.get('whiteList').then(showWhiteList);
 	storage.get('disableNotifications').then(setCurrentChoice);
 }
 
